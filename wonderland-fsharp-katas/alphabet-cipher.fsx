5bb0de5d4386f6f2eec20f12d4fcbac9716c545b
Ryan Kenavey <ryan@olo.com>
2021-08-16 17:42:50 -0400

    implement encode and decode

--- wonderland-fsharp-katas/alphabet-cipher.fsx
+++ wonderland-fsharp-katas/alphabet-cipher.fsx
@@ -1,13 +1,51 @@
 // See the file alphabet-cipher.md for detailed information.
 
+open System
+
 type Message = string
 type Keyword = string
 
+let aAsciiValue = 97
+
+let letterToIndex (letter:char) =
+    letter
+    |> Convert.ToInt32
+    |> (fun i -> i - aAsciiValue)
+
+let indexToLetter (index:int) =
+    index
+    |> (fun i -> i + aAsciiValue)
+    |> Convert.ToChar
+
+let matrix =
+    Array2D.init 26 26 (fun i j -> (i + j) % 26 |> indexToLetter)
+
+let createMessageKeyword (key:Keyword) (desiredlength:int) : Keyword =
+    let replicateTimes =
+        (float desiredlength) / (float key.Length) 
+        |> Math.Ceiling
+        |> int
+    key
+    |> String.replicate replicateTimes
+    |> (fun s -> s.Substring(0, desiredlength))
+
 let encode (key:Keyword) (message:Message) : Message =
-    "encodeme"
+    let messageKeyword =
+        createMessageKeyword key message.Length
+        |> (fun s -> s.ToCharArray())
+    Array.zip (message.ToCharArray()) messageKeyword
+        |> Array.map (fun (m, k) -> matrix.[(letterToIndex m), (letterToIndex k)] )
+        |> (fun a -> String(a))
 
 let decode (key:Keyword) (message:Message) : Message =
-    "decodeme"
+    let messageKeyword =
+        createMessageKeyword key message.Length
+        |> (fun s -> s.ToCharArray())
+    Array.zip (message.ToCharArray()) messageKeyword
+        |> Array.map (fun (m, k) -> matrix.[(letterToIndex k), *]
+                                    |> Array.findIndex(fun c -> c = m)
+                                    |> indexToLetter)
+        |> (fun a -> String(a))
 
 let decipher (cipher:Message) (message:Message) : Keyword =
     "decypherme"
@@ -16,6 +54,18 @@ let decipher (cipher:Message) (message:Message) : Keyword =
 open Swensen.Unquote
 
 let tests () =
+
+    // verify letter to index and vice versa
+    test <@ letterToIndex 'a' = 0 @>
+    test <@ letterToIndex 'z' = 25 @>
+    test <@ indexToLetter 0 = 'a' @>
+    test <@ indexToLetter 25 = 'z' @>
+
+    // verify creating the key for a specific message
+    test <@ createMessageKeyword "abc" 1 = "a" @>
+    test <@ createMessageKeyword "abc" 3 = "abc" @>
+    test <@ createMessageKeyword "abc" 6 = "abcabc" @>
+    test <@ createMessageKeyword "abc" 7 = "abcabca" @>
 
     // verify encoding
     test <@ encode "vigilance" "meetmeontuesdayeveningatseven" = "hmkbxebpxpmyllyrxiiqtoltfgzzv" @>
